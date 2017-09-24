4f9a0791869d2a33187d5209249d3b589a436bf4
mohamed <mohamed@Gouti>
2017-09-24 09:54:00 +0100

    Commit TestScalaAuth

---
+++ src/module/src/main/scala/jp/t2v/lab/play2/auth/AuthConfig.scala
@@ -0,0 +1,60 @@
+package jp.t2v.lab.play2.auth
+
+import play.api.mvc._
+import scala.reflect.{ClassTag, classTag}
+import scala.concurrent.{ExecutionContext, Future}
+
+trait AuthConfig {
+
+  type Id
+
+  type User
+
+  type Authority
+
+  implicit def idTag: ClassTag[Id]
+
+  def sessionTimeoutInSeconds: Int
+
+  def resolveUser(id: Id)(implicit context: ExecutionContext): Future[Option[User]]
+
+  def loginSucceeded(request: RequestHeader)(implicit context: ExecutionContext): Future[Result]
+
+  def logoutSucceeded(request: RequestHeader)(implicit context: ExecutionContext): Future[Result]
+
+  def authenticationFailed(request: RequestHeader)(implicit context: ExecutionContext): Future[Result]
+
+  def authorizationFailed(request: RequestHeader, user: User, authority: Option[Authority])(implicit context: ExecutionContext): Future[Result]
+
+  def authorize(user: User, authority: Authority)(implicit context: ExecutionContext): Future[Boolean]
+
+  lazy val idContainer: AsyncIdContainer[Id] = AsyncIdContainer(new CacheIdContainer[Id])
+
+  @deprecated("it will be deleted since 0.14.x. use CookieTokenAccessor constructor", since = "0.13.1")
+  final lazy val cookieName: String = throw new AssertionError("use tokenAccessor setting instead.")
+
+  @deprecated("it will be deleted since 0.14.0. use CookieTokenAccessor constructor", since = "0.13.1")
+  final lazy val cookieSecureOption: Boolean = throw new AssertionError("use tokenAccessor setting instead.")
+
+  @deprecated("it will be deleted since 0.14.0. use CookieTokenAccessor constructor", since = "0.13.1")
+  final lazy val cookieHttpOnlyOption: Boolean = throw new AssertionError("use tokenAccessor setting instead.")
+
+  @deprecated("it will be deleted since 0.14.0. use CookieTokenAccessor constructor", since = "0.13.1")
+  final lazy val cookieDomainOption: Option[String] = throw new AssertionError("use tokenAccessor setting instead.")
+
+  @deprecated("it will be deleted since 0.14.0. use CookieTokenAccessor constructor", since = "0.13.1")
+  final lazy val cookiePathOption: String = throw new AssertionError("use tokenAccessor setting instead.")
+
+  @deprecated("it will be deleted since 0.14.0. use CookieTokenAccessor constructor", since = "0.13.1")
+  final lazy val isTransientCookie: Boolean = throw new AssertionError("use tokenAccessor setting instead.")
+
+  lazy val tokenAccessor: TokenAccessor = new CookieTokenAccessor(
+    cookieName = "PLAY2AUTH_SESS_ID",
+    cookieSecureOption = play.api.Play.maybeApplication.exists(app => play.api.Play.isProd(app)),
+    cookieHttpOnlyOption = true,
+    cookieDomainOption = None,
+    cookiePathOption = "/",
+    cookieMaxAge = Some(sessionTimeoutInSeconds)
+  )
+
+}
---
+++ src/project/plugins.sbt
@@ -0,0 +1,11 @@
+// The Typesafe repository
+resolvers ++= Seq(
+  "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/",
+  "Typesafe Snapshots repository" at "http://repo.typesafe.com/typesafe/snapshots/"
+)
+
+// Use the Play sbt plugin for Play projects
+addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.4.6")
+
+scalacOptions ++= Seq("-deprecation", "-unchecked", "-language:_")
+
